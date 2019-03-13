b837bce0da44ff4e5838a4b84b2e461585d9185e
ms0223900 <ms0223900@gmail.com>
2019-03-13 17:48:21 +0800

    190313_update QAs star

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { SingleQA } from './singleQA'
 
+// let starLS = localStorage.getItem('starID')
+// let noteLS = localStorage.getItem('IdNote')
+// console.log(JSON.parse(noteLS))
+const LS = localStorage
 const QAs = [
   {
     id: 'Q1',
@@ -35,7 +39,18 @@ const QAs = [
     note: '',
   },
 ]
-
+const toggleOneInArrObj = (targetAttr, arrObj, conditionFN) => {
+  const tarObj = arrObj.filter(conditionFN)[0]
+   let newArrObj = [ 
+    ...arrObj.filter(!conditionFN),
+    {
+      ...tarObj,
+      [targetAttr]: tarObj[targetAttr] ? false : true
+    }
+  ]
+  console.log(newArrObj)
+  return newArrObj
+}
 
 export default class App extends React.Component {
   constructor(props) {
@@ -46,31 +61,33 @@ export default class App extends React.Component {
     };
   }
   componentWillMount = () => {
+    // const updateFromLS_QAs = QAs.map(qa => qa = qa)
     const Q = QAs.map(q => q = {
       id: q.id, 
       answer: '', 
       correctAnswer: q.answer, 
       checked: 'notYet',
       star: q.star,
+      note: q.note,
     })
     this.setState({
       myAnswer: Q
     })
     
+    if(!LS.getItem('QAstorge')) {
+      LS.setItem('QAstorge', JSON.stringify(QAs))
+    }
+    //init localStorage
+    // starLS = starLS ? starLS : localStorage.setItem('starID', '')
+
+    // noteLS = noteLS ? noteLS : localStorage.setItem('IdNote', JSON.stringify({id: 'Q1', note: 'aaa'}))
+
   }
   
   _handleCheckAnswer = () => {
     const { myAnswer } = this.state
-    const checkedAnswer = myAnswer.map(a => a = {...a, checked: a.answer === a.correctAnswer ? true : false } )
+    const checkedAnswer = myAnswer.map(a => a = {...a, checked: a.answer === a.correctAnswer ? true : false })
     console.log(checkedAnswer)
-    // for (let i = 0; i < myAnswer.length; i++) {
-    //   if(myAnswer[i]['answer'] === QAs[i]['answer']) {
-    //     myAnswer[i]['checked'] = true
-    //   } else {
-    //     myAnswer[i]['checked'] = false
-    //   }
-    // }
-    // console.log(myAnswer)
     this.setState({
       isHandIn: true,
       myAnswer: checkedAnswer
@@ -92,16 +109,35 @@ export default class App extends React.Component {
         ]
       })
     }
-    
   }
+  _handelStar = (e) => {
+    const { myAnswer } = this.state
+    const { name } = e.target
+    // const starAnswer = myAnswer.filter(a => a.id === name)[0]
+    // const staredMyAnswer = [ 
+    //   ...myAnswer.filter(a => a.id !== name),
+    //   {
+    //     ...starAnswer,
+    //     star: starAnswer.star ? false : true
+    //   }
+    // ]
+    const staredMyAnswer = toggleOneInArrObj('star', myAnswer, a => a.id === name)
+    this.setState({
+      myAnswer: staredMyAnswer,
+    })
+    console.log(this.state.myAnswer)
+    localStorage.setItem('QAstorge', QAs)
+  }
+
   render() {
-    const { myAnswer, isHandIn } = this.state
+    const { myAnswer, isHandIn,  } = this.state
     return (
       <div>
         Hi
         {QAs.map(qa => 
           <SingleQA
             changeAnswer ={this._handleChangeAnswer}
+            starIt={this._handelStar}
             myAnswer={myAnswer}
             id={qa.id}
             question={qa.question}
--- src/singleQA.js
+++ src/singleQA.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
+const styles = {
+  starBTN: {
+    normal: {
+      'backgroundColor': '#ddd',
+      'color': '#111'
+    },
+    active: {
+      'backgroundColor': '#333',
+      'color': '#fff'
+    }
+  }
+}
+
 
 export class SingleQA extends React.Component {
   constructor(props) {
@@ -7,8 +20,12 @@ export class SingleQA extends React.Component {
     this.state = {};
   }
   render() {
+    const { starBTN } = styles
+
+
     const { 
-      changeAnswer, 
+      changeAnswer,
+      starIt, 
       myAnswer=[], 
       id=1, 
       question='', 
@@ -19,16 +36,21 @@ export class SingleQA extends React.Component {
     const thisAnswer = myAnswer.filter(a => a.id === id)[0]
     return (
       <div>
-        <form onChange={changeAnswer} id={id}>
+        <form onChange={changeAnswer} id={id} onSubmit={e => e.preventDefault()}>
           <h3>{id} <span>{thisAnswer.checked !== 'notYet' ? (thisAnswer.checked ? '✔' : '✘') : ''}</span> </h3>
           <p>{question}</p>
           <span>{thisAnswer.answer}</span>
           {options.map(op => 
             <label>
-              <input type='radio' name={id} value={op} disabled={isHandIn}/>{op}
+              <input 
+                type='radio' 
+                name={id} 
+                value={op}
+                disabled={isHandIn} 
+              /> {op}
             </label>
           )}
-          <button>STAR</button>
+          <button onClick={starIt} name={id} style={ thisAnswer.star ? starBTN.active : starBTN.normal} >STAR</button>
           <button>NOTE</button>
         </form>
       </div>
